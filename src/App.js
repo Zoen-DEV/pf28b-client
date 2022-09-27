da326642276e0bb964f25c9667513f103c78cdba
Zoen-DEV <enzoholgadodev@gmail.com>
2022-09-27 12:17:52 -0500

    implementando footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
 import Nav from "./components/Nav"
+import { getAllAnimes } from "./redux/actions";
 
 function App() {
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    dispatch(getAllAnimes())
+  }, [dispatch])
+  
+
   return (
     <div className="App">
       <Nav></Nav>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import LOGOdemo from '../assets/LOGOdemo.png'
+import React from "react";
+import { Link } from "react-router-dom";
+import LOGOdemo from "../assets/LOGOdemo.png";
 
 const Footer = () => {
   return (
     <footer>
-        <Link className='link' to='/about'>ABOUT</Link>
+      <Link className="link" to="/about">
+        ABOUT
+      </Link>
+      <Link to="/">
         <img src={LOGOdemo} alt="animercce" />
-        <Link className='link' to='/contactus'>CONTACT US</Link>
+      </Link>
+      <Link className="link" to="/contactus">
+        CONTACT US
+      </Link>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
