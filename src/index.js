23364bf99476a2adeb6f0fa529a9ab2ba9479ab4
Zoen-DEV <enzoholgadodev@gmail.com>
2022-09-26 22:29:38 -0500

    instalando sass, modularizando los primeros componentes de estilos y agregando variables de colores

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import rootReducer from "./redux/reducers";
-import "./index.css";
+import "./styles/index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
