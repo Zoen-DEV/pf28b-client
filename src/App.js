b6c6cfa00313b278781cf727739a0e21a24f7afc
Pedro <pedro.saravia.q@uni.pe>
2022-09-30 10:32:52 -0500

    login component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Footer from "./components/Footer";
 import Home from "./components/Home";
 import Nav from "./components/Nav";
 import Details from "./components/Details";
+import SignUp from "./components/SignUp";
+import LogIn from "./components/LogIn";
 
 function App() {
   return (
@@ -10,7 +12,9 @@ function App() {
       <Nav></Nav>
       <Routes>
         <Route index element={<Home></Home>} />
-        <Route path="/details/:id" element={<Details></Details>}/>
+        <Route path="/details/:id" element={<Details></Details>} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<LogIn />} />
       </Routes>
       <Footer></Footer>
     </div>
