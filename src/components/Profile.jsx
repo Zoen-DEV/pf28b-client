2a73451a841a7c96ef0ba3e56e5524ca5b06223d
ArmandoBGarcia <xdarcx@hotmail.es>
2022-10-09 01:43:49 -0500

    showing the user info in the profile route

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,13 +1,40 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { logOut } from "../redux/Actions/actions";
+
 
 const Profile = () => {
-  // const dispatch = useDispatch();
-  const user = useSelector((state) => state.user);
-  console.log({ user });
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const userActive = localStorage.getItem("user");
+  const user = JSON.parse(userActive)
   return (
     <div>
-      <h3>{user.username}</h3>
+        <button
+          onClick={() => {
+            dispatch(logOut());
+            navigate("/");
+          }}
+        >
+          LogOut
+        </button>
+      <h1>Wellcome to your profile</h1>
+      <div>
+      <h4>Name: </h4>
+      <p>{user.username}</p>
+      </div>
+      <div>
+      <img src={user.image} alt={user.username} />
+      </div>
+      <div>
+      <h4>Email: </h4>
+      <p>{user.email}</p>
+      </div>
+      <div>
+      <h4>Cellphone: </h4>
+      {!user.cellphone ? <p>N/A</p> : user.cellphone}
+      </div>
     </div>
   );
 };
