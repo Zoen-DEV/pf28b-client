7e7b2d1950022747628b7a62f5b8fa437d4c225b
Zoen-DEV <enzoholgadodev@gmail.com>
2022-10-11 11:50:07 -0500

    arreglando conflictos

--- src/components/RequireAuth.jsx
+++ src/components/RequireAuth.jsx
@@ -3,7 +3,7 @@ import { Outlet, Navigate, useLocation } from "react-router-dom";
 
 function RequireAuth() {
   const location = useLocation();
-  console.log(localStorage.getItem("token"));
+  // console.log(localStorage.getItem("token"));
   return localStorage.getItem("token") ? <Outlet /> : <Navigate to={"/login"} state={{ from: location }} replace />;
 }
 
